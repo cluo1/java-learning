128fb5e0b78500ebd6588f3f96881fffd549d6e9
邱建东 <qiujiandong@talkmoney.cn>
2018-08-24 00:09:12 +0800

    add dao

---
+++ nearjob/src/main/java/cn/mariojd/nearjob/dao/AndroidDao.java
@@ -0,0 +1,12 @@
+package cn.mariojd.nearjob.dao;
+
+import cn.mariojd.nearjob.base.BaseDao;
+import cn.mariojd.nearjob.entity.Android;
+
+/**
+ * @author Jared
+ * @date 2018/8/23 17:41
+ */
+
+public interface AndroidDao extends BaseDao<Android> {
+}
---
+++ nearjob/src/main/java/cn/mariojd/nearjob/dao/PhpDao.java
@@ -0,0 +1,11 @@
+package cn.mariojd.nearjob.dao;
+
+import cn.mariojd.nearjob.base.BaseDao;
+import cn.mariojd.nearjob.entity.PHP;
+
+/**
+ * @author Jared
+ * @date 2018/8/23 17:41
+ */
+public interface PhpDao extends BaseDao<PHP> {
+}
