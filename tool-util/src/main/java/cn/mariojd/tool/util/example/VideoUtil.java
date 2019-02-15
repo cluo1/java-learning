5f8be9b4ac4d5446db9b0c61759bb072d6e00784
happyJared <happyjared1995@gmail.com>
2019-02-15 11:43:29 +0800

    update tool-util

--- tool-util/src/main/java/cn/mariojd/tool/util/example/VideoUtil.java
+++ tool-util/src/main/java/cn/mariojd/tool/util/example/VideoUtil.java
@@ -1,73 +1,69 @@
 package cn.mariojd.tool.util.example;
 
+import com.sun.org.apache.bcel.internal.util.ClassPath;
 import org.bytedeco.javacv.FFmpegFrameGrabber;
 import org.bytedeco.javacv.Frame;
 import org.bytedeco.javacv.FrameGrabber;
 import org.bytedeco.javacv.Java2DFrameConverter;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.util.ResourceUtils;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 
 /**
  * @author Jared
- * @date 2019/2/15 10:48
+ * @date 2019/2/14 20:48
  */
 public class VideoUtil {
 
     /**
-     * 获取指定视频的帧并保存为图片至指定目录
+     * 截取视频获得指定帧的图片
      *
-     * @param video         源视频文件
-     * @param thumbnailPath 截取帧缩略图图片的存放路径
+     * @param video   源视频文件
+     * @param picPath 截图存放路径
      */
-    public static void getVideoThumbnail(File video, String thumbnailPath) {
+    public static void getVideoPic(File video, String picPath) {
         FFmpegFrameGrabber ff = new FFmpegFrameGrabber(video);
         try {
             ff.start();
-        } catch (FrameGrabber.Exception e) {
-            e.printStackTrace();
-        }
-        int length = ff.getLengthInFrames();
 
-        int i = 0;
-        int maxFrame = length / 2;
-        Frame frame = null;
-        while (i < length) {
-            // 过滤前5帧，避免出现全黑的图片，依自己情况而定
-            try {
+            // 截取中间帧图片(具体依实际情况而定)
+            int i = 0;
+            int length = ff.getLengthInFrames();
+            int middleFrame = length / 2;
+            Frame frame = null;
+            while (i < length) {
                 frame = ff.grabFrame();
-                if ((i > maxFrame) && (frame.image != null)) {
+                if ((i > middleFrame) && (frame.image != null)) {
                     break;
                 }
                 i++;
-            } catch (FrameGrabber.Exception e) {
-                e.printStackTrace();
             }
-        }
 
-        Java2DFrameConverter converter = new Java2DFrameConverter();
-        BufferedImage srcBi = converter.getBufferedImage(frame);
-        int imgWidth = srcBi.getWidth();
-        int imgHeight = srcBi.getHeight();
-        // 对截取的帧进行等比例缩放
-        int width = 480;
-        int height = (int) (((double) width / imgWidth) * imgHeight);
-        BufferedImage bi = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
-        bi.getGraphics().drawImage(srcBi.getScaledInstance(width, height, Image.SCALE_SMOOTH), 0, 0, null);
+            // 截取的帧图片
+            Java2DFrameConverter converter = new Java2DFrameConverter();
+            BufferedImage srcImage = converter.getBufferedImage(frame);
+            int srcImageWidth = srcImage.getWidth();
+            int srcImageHeight = srcImage.getHeight();
 
-        File targetFile = new File(thumbnailPath);
-        try {
-            ImageIO.write(bi, "jpg", targetFile);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try {
+            // 对截图进行等比例缩放(缩略图)
+            int width = 480;
+            int height = (int) (((double) width / srcImageWidth) * srcImageHeight);
+            BufferedImage thumbnailImage = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
+            thumbnailImage.getGraphics().drawImage(srcImage.getScaledInstance(width, height, Image.SCALE_SMOOTH), 0, 0, null);
+
+            File picFile = new File(picPath);
+            ImageIO.write(thumbnailImage, "jpg", picFile);
+
             ff.stop();
-        } catch (FrameGrabber.Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
@@ -75,7 +71,7 @@ public class VideoUtil {
     /**
      * 获取视频时长，单位为秒
      *
-     * @param video
+     * @param video 源视频文件
      * @return 时长（s）
      */
     public static long getVideoDuration(File video) {
@@ -89,6 +85,16 @@ public class VideoUtil {
             e.printStackTrace();
         }
         return duration;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        String videoPath = ResourceUtils.CLASSPATH_URL_PREFIX + "video.mp4";
+        File video = ResourceUtils.getFile(videoPath);
+        String picPath = "video.jpg";
+        getVideoPic(video, picPath);
+
+        long duration = getVideoDuration(video);
+        System.out.println("videoDuration = " + duration);
     }
 
 }
