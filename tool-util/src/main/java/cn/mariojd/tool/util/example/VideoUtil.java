a518013eac9f2b2c22c51623b2c89e22c1c21caf
happyJared <happyjared1995@gmail.com>
2019-03-15 16:38:55 +0800

    feat: 新增其它类型视频文件转换成MP4格式功能

--- tool-util/src/main/java/cn/mariojd/tool/util/example/VideoUtil.java
+++ tool-util/src/main/java/cn/mariojd/tool/util/example/VideoUtil.java
@@ -1,9 +1,8 @@
 package cn.mariojd.tool.util.example;
 
-import org.bytedeco.javacv.FFmpegFrameGrabber;
+import org.bytedeco.javacpp.avcodec;
+import org.bytedeco.javacv.*;
 import org.bytedeco.javacv.Frame;
-import org.bytedeco.javacv.FrameGrabber;
-import org.bytedeco.javacv.Java2DFrameConverter;
 import org.springframework.util.ResourceUtils;
 
 import javax.imageio.ImageIO;
@@ -19,6 +18,10 @@ import java.io.IOException;
  * @date 2019/2/14 20:48
  */
 public class VideoUtil {
+
+    private static final String JPG = "jpg";
+
+    private static final String MP4_SUFFIX = ".mp4";
 
     /**
      * 截取视频获得指定帧的图片
@@ -51,13 +54,13 @@ public class VideoUtil {
             int srcImageHeight = srcImage.getHeight();
 
             // 对截图进行等比例缩放(缩略图)
-            int width = 480;
+            final int width = 480;
             int height = (int) (((double) width / srcImageWidth) * srcImageHeight);
             BufferedImage thumbnailImage = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
             thumbnailImage.getGraphics().drawImage(srcImage.getScaledInstance(width, height, Image.SCALE_SMOOTH), 0, 0, null);
 
             File picFile = new File(picPath);
-            ImageIO.write(thumbnailImage, "jpg", picFile);
+            ImageIO.write(thumbnailImage, JPG, picFile);
 
             ff.stop();
         } catch (IOException e) {
@@ -82,6 +85,51 @@ public class VideoUtil {
             e.printStackTrace();
         }
         return duration;
+    }
+
+    /**
+     * 其它视频文件转换成 MP4 格式
+     *
+     * @param video
+     */
+    public static void transferToMP4(File video) {
+        String[] paths = video.getPath().split("\\.");
+        if (!MP4_SUFFIX.contains(paths[paths.length - 1])) {
+            String mp4Path = paths[0] + MP4_SUFFIX;
+            FFmpegFrameGrabber ff = new FFmpegFrameGrabber(video);
+            FFmpegFrameRecorder recorder;
+
+            try {
+                ff.start();
+                recorder = new FFmpegFrameRecorder(mp4Path, ff.getImageWidth(), ff.getImageHeight(),
+                        ff.getAudioChannels());
+                recorder.setVideoCodec(avcodec.AV_CODEC_ID_H264);
+                recorder.setFrameRate(ff.getFrameRate());
+                recorder.setSampleRate(ff.getSampleRate());
+                recorder.setFrameRate(ff.getFrameRate());
+                recorder.start();
+                Frame capturedFrame;
+
+                while (true) {
+                    try {
+                        capturedFrame = ff.grabFrame();
+                        if (capturedFrame == null) {
+                            break;
+                        }
+                        recorder.setTimestamp(ff.getTimestamp());
+                        recorder.record(capturedFrame);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                recorder.stop();
+                recorder.release();
+                ff.stop();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public static void main(String[] args) {
